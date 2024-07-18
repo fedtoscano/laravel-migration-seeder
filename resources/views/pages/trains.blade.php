bc9c88efbd70418c11cf3581567e9c4fcd7d04ec
fedtoscano <federicotoscano611@gmail.com>
2024-07-18 16:26:37 +0200

    modificato layout con tabella di bootstrap

--- resources/views/pages/trains.blade.php
+++ resources/views/pages/trains.blade.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Trains</title>
+
+    @vite('resources/js/app.js')
 </head>
 <body>
     <h1>Lista dei Treni</h1>
-    <table>
+    {{-- <table>
         <thead>
             <tr>
                 <th>Azienda</th>
@@ -36,6 +38,37 @@
                     <td>{{ $train->cancellato ? 'Sì' : 'No' }}</td>
                 </tr>
             @endforeach
+        </tbody>
+    </table> --}}
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Azienda</th>
+                <th scope="col">Stazione di Partenza</th>
+                <th scope="col">Stazione di Arrivo</th>
+                <th scope="col">Orario di Partenza</th>
+                <th scope="col">Orario di Arrivo</th>
+                <th scope="col">Codice Treno</th>
+                <th scope="col">Numero Carrozze</th>
+                <th scope="col">In Orario</th>
+                <th scope="col">Cancellato</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($trains as $train)
+            <tr>
+                <td>{{ $train->azienda }}</td>
+                <td>{{ $train->stazione_partenza }}</td>
+                <td>{{ $train->stazione_arrivo }}</td>
+                <td>{{ $train->orario_partenza }}</td>
+                <td>{{ $train->orario_arrivo }}</td>
+                <td>{{ $train->codice_treno }}</td>
+                <td>{{ $train->numero_carrozze }}</td>
+                <td>{{ $train->in_orario ? 'Sì' : 'No' }}</td>
+                <td>{{ $train->cancellato ? 'Sì' : 'No' }}</td>
+            </tr>
+        @endforeach
         </tbody>
     </table>
 </body>
